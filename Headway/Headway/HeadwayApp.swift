0985037b680ecdeda7364aef261e0363a4a07094
AnieStudio <113438184+AnieStudio@users.noreply.github.com>
2023-12-23 13:00:47 +0200

    Initial commit with empty ptoj

---
+++ Headway/Headway/HeadwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayApp.swift
+//  Headway
+//
+//  Created by Hlib Serediuk-Personal on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeadwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
